eaab513e6b3be3dbb1d2ca184cb0728faae9af97
George <george.circleci.com>
2016-10-21 16:03:43 -0700

    changes to only build ios as a build target

--- build.sh
+++ build.sh
@@ -3,19 +3,29 @@
 # Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
 
 # Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
+project="Simple Mobile Placeholder"
 
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+# echo "Attempting to build $project for Windows"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+#   -quit
 
-echo "Attempting to build $project for OS X"
+# echo "Attempting to build $project for OS X"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+#   -quit
+
+echo "Attempting to build $project for iOS"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
@@ -23,17 +33,20 @@ echo "Attempting to build $project for OS X"
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
+  -buildTarget ios \
+  -runEditorTests
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
+mv 
+
+# echo "Attempting to build $project for Linux"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+#   -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
