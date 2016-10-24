0d81c8e68f4a57042fa413b7c8113444a98b5d7d
George <george.circleci.com>
2016-10-24 11:30:35 -0700

    Added download link for ios build support according to buildurl pattern

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-#curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
-curl -o Unity.pkg http://download.unity3d.com/download_unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.1f1.pkg
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
+curl -o Unity-iOS.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-5.4.2f2.pkg
+#curl -o Unity.pkg http://download.unity3d.com/download_unity/649f48bbbf0f/MacEditorInstaller/Unity-5.4.1f1.pkg
 sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity-iOS.pkg -target /
