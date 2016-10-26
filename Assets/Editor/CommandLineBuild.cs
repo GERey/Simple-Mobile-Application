247e1e865c0c716dc5f52367f882caecb2a7e70b
George <george.circleci.com>
2016-10-26 16:00:59 -0700

    Commiting the actual things that will do the command line builds that work locally

---
+++ Assets/Editor/CommandLineBuild.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class CommandLineBuild {
+	[MenuItem("File/CommandLineBuild/iOS")]
+	public static void iOSBuild ()
+	{
+        	EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iOS);
+        		// var scenes = "Assets/Scenes/Main.unity";
+
+                List<EditorBuildSettingsScene> editorBuildSettingsScenes = new List<EditorBuildSettingsScene>();
+                // foreach (var sceneAsset in m_SceneAssets)
+                // {
+                //     string scenePath = AssetDatabase.GetAssetPath (sceneAsset);
+                //     if (!string.IsNullOrEmpty (scenePath))
+                //         editorBuildSettingsScenes.Add (new EditorBuildSettingsScene (scenePath, true));
+                // }
+
+                string scenePath = "Assets/Scenes/Main.unity";
+                editorBuildSettingsScenes.Add (new EditorBuildSettingsScene (scenePath, true));
+
+        	var opts = BuildOptions.None;
+
+        	BuildPipeline.BuildPlayer(editorBuildSettingsScenes.ToArray(), "Builds/iOS", BuildTarget.iOS, opts);
+	}
+}
+
+
+// using UnityEditor;
+// class CommandLineBuild
+// {
+//      static void PerformBuild ()
+//      {
+//          string[] scenes = { "Assets/MyScene.unity" };
+//          BuildPipeline.BuildPlayer(scenes, ...);
+//      }
+// }
