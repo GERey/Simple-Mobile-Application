140b11e20c82b6addfbde5f56fe173afbb17ee22
George <george.circleci.com>
2016-10-28 14:13:29 -0700

    changed project settings to only require simulator, changed build.sh to upload the .app

--- build.sh
+++ build.sh
@@ -37,6 +37,9 @@ echo "Attempting to build $project for iOS"
   -executeMethod CommandLineBuild.iOSBuild \
   -quit
 
+-echo 'Logs from build'
+-cat $(pwd)/unity.log 
+
 echo "returning the license"
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -50,12 +53,13 @@ echo "returning the license"
   -projectPath $(pwd) \
   -quit
 
+-echo 'Logs from build returning license'
+-cat $(pwd)/unity.log 
+
 echo "moving the ios build to somewhere"
 
-mv Builds/iOS $CIRCLE_ARTIFACTS
-
-
-
+cd Builds/iOS
+xcodebuild -project Unity-iPhone.xcodeproj -scheme Unity-iPhone clean build CONFIGURATION_BUILD_DIR=$CIRCLE_ARTIFACTS
 
 
 
