ca741c49fa39e538e5e6da288085ec83dba17640
George <george.circleci.com>
2016-10-28 17:19:37 -0700

    cleaning up some files

--- build.sh
+++ build.sh
@@ -1,29 +1,6 @@
 #! /bin/sh
 
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# Change this the name of your project. This will be the name of the final executables as well.
 project="Simple Mobile Placeholder"
-
-# echo "Attempting to build $project for Windows"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#   -batchmode \
-#   -nographics \
-#   -silent-crashes \
-#   -logFile $(pwd)/unity.log \
-#   -projectPath $(pwd) \
-#   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-#   -quit
-
-# echo "Attempting to build $project for OS X"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#   -batchmode \
-#   -nographics \
-#   -silent-crashes \
-#   -logFile $(pwd)/unity.log \
-#   -projectPath $(pwd) \
-#   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-#   -quit
 
 echo "Attempting to build $project for iOS"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -58,17 +35,5 @@ cat $(pwd)/unity.log
 
 cd Builds/iOS
 xcodebuild -project Unity-iPhone.xcodeproj -scheme Unity-iPhone clean build CONFIGURATION_BUILD_DIR=$CIRCLE_ARTIFACTS
-
-
-
-# echo "Attempting to build $project for Linux"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#   -batchmode \
-#   -nographics \
-#   -silent-crashes \
-#   -logFile $(pwd)/unity.log \
-#   -projectPath $(pwd) \
-#   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-#   -quit
 
 
